73f18812ead525a254289735f8b496faa773a29f
rafaelfmonteiro <rafius@gmail.com>
2018-09-30 09:52:42 -0300

    ErrorHandler, tratamento de erros

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -2,9 +2,11 @@ import {Injectable} from "@angular/core"
 import {Http} from '@angular/http'
 import {Observable} from 'rxjs/Observable'
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 import {Restaurant} from "./restaurant/restaurant.model"
 import {MEAT_API} from "../app.api"
+import {ErrorHandler} from "../app.error-handler"
 
 @Injectable()
 export class RestaurantsService {
@@ -12,7 +14,8 @@ export class RestaurantsService {
     constructor(private http :Http){}
 
     restaurants(): Observable<Restaurant[]> {
-      // return this.rests
-      return this.http.get(`${MEAT_API}/restaurants`).map(response => response.json())
+      return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
