b2bd8753f110f554cbd927e3fd5d3a2c53e63729
rafaelfmonteiro <rafius@gmail.com>
2018-10-06 09:32:20 -0300

    Finalizando componentes do carrinho e avaliação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,9 @@ import { OrderItemsComponent } from './order/order-items/order-items.component';
 import { RestaurantsService } from './restaurants/restaurants.service';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderService } from './order/order.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderSummaryComponent } from './order-summary/order-summary.component';
+import { RatingComponent } from './shared/rating/rating.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +45,10 @@ import { OrderService } from './order/order.service';
     OrderComponent,
     InputComponent,
     RadioComponent,
-    OrderItemsComponent
+    OrderItemsComponent,
+    DeliveryCostsComponent,
+    OrderSummaryComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/order/order.model.ts
@@ -0,0 +1,18 @@
+ class Order {
+
+   constructor (
+     public address:string,
+     public number: number,
+     public optionalAddress: string,
+     public paymentOption: string,
+     public orderItems: OrderItem[]
+   ){}
+
+ }
+
+ class OrderItem {
+   constructor(public quantity:number, public menuId:String){}
+ }
+
+
+export {Order, OrderItem}
