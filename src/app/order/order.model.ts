75ababf5eced73aa9f630554aab6019a2bfd4ba1
rafaelfmonteiro <rafius@gmail.com>
2018-11-04 20:28:38 -0200

    Refactoring Angular 4.3

--- src/app/order/order.model.ts
+++ src/app/order/order.model.ts
@@ -1,11 +1,13 @@
  class Order {
 
+
    constructor (
      public address:string,
      public number: number,
      public optionalAddress: string,
      public paymentOption: string,
-     public orderItems: OrderItem[]
+     public orderItems: OrderItem[],
+     public id?: string
    ){}
 
  }
