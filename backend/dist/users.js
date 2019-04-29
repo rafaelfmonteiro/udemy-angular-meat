516f557edc99ab287b3d4fc40cdc0b73e4d81058
rafaelfmonteiro <rafius@gmail.com>
2019-04-29 14:16:35 -0300

    Segurança Backend / JWT

---
+++ backend/dist/users.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var User = (function () {
+    function User(email, name, password) {
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+    User.prototype.matches = function (another) {
+        return another !== undefined &&
+            another.email === this.email &&
+            another.password === this.password;
+    };
+    return User;
+}());
+exports.User = User;
+exports.users = {
+    "rafius@gmail.com": new User('rafius@gmail.com', 'Rafael Monteiro', 'rafa')
+};
---
+++ backend/server.ts
@@ -0,0 +1,31 @@
+import * as jsonServer from 'json-server'
+import {Express} from 'express'
+
+import * as fs from 'fs'
+import * as https from 'https'
+
+import {handleAuthentication} from './auth'
+
+const server: Express = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+// Set default middlewares (logger, static, cors and no-cache)
+server.use(middlewares)
+
+server.use(jsonServer.bodyParser)
+
+//middlewares para login
+server.post('/login', handleAuthentication )
+
+// Use default router
+server.use(router)
+
+const options = {
+  cert : fs.readFileSync('./backend/keys/cert.pem'),
+  key : fs.readFileSync('./backend/keys/key.pem')
+}
+
+https.createServer(options, server).listen(3001, () => {
+  console.log('JSON Server is running https://localhost:3001')
+})
